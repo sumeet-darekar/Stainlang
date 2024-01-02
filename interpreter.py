c7a921a1e849167edabe58b29436187229acf33c
sumeet <sumitdarekar2003@gmail.com>
2024-01-02 23:42:26 +0530

    baseModel completed

--- interpreter.py
+++ interpreter.py
@@ -1,1 +1,47 @@
-print("hi")
+from lexer import Integer,Float
+
+class Interpreter:
+    def __init__(self,tree):
+        self.tree=tree
+
+    def readINT(self,value):
+        return int(value)
+    
+    def readFLOAT(self,value):
+        return float(value)
+
+    def computeBin(self, left, operator, right):
+        left_type = left.dataType
+        right_type = right.dataType
+
+        left = getattr(self, f"read{left_type}")(left.value)
+        right = getattr(self, f"read{right_type}")(right.value)
+
+        if operator.value == "+":
+            output = left + right
+        elif operator.value == "-":
+            output = left - right
+        elif operator.value == "*":
+            output = left * right
+        elif operator.value == "/":
+            output = left / right
+        
+        return Integer(output) if (left_type == "INT" and right_type == "INT") else Float(output) 
+
+    def interpret(self,tree=None):
+
+        if tree is None:
+            tree = self.tree
+
+        leftNode = tree[0]
+        if isinstance(leftNode, list):
+            leftNode = self.interpret(leftNode)
+        
+        rightNode = tree[2] 
+        if isinstance(rightNode, list):
+            rightNode = self.interpret(rightNode)
+
+        operator = tree[1]
+
+        return self.computeBin(leftNode, operator, rightNode)
+
--- stain.py
+++ stain.py
@@ -1,11 +1,17 @@
 from lexer import Lexer
 from parser import Parser
+from interpreter import Interpreter
+
 
 while True:
-    text=input("stainLang>> ")
-    tokenizer=Lexer(text)
-    tokens=tokenizer.tokenization()
-    print(tokens)
+    text = input("stainLang>> ")
+    tokenizer = Lexer(text)
+    tokens = tokenizer.tokenization()
+   
     parser = Parser(tokens)
     tree = parser.parse()
-    print(tree)
+    
+    interpreter = Interpreter(tree)
+    output = interpreter.interpret()
+
+    print(output)
